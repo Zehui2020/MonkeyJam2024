ea17a438131ead736b4c68191fdea0b52d9aa304
Zehui2020 <zehui2020@gmail.com>
2024-06-15 22:51:37 +0800

    Added font

--- Assets/Scripts/Player/MovementController.cs
+++ Assets/Scripts/Player/MovementController.cs
@@ -4,23 +4,5 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D frontTireRB;
-    [SerializeField] private Rigidbody2D backTireRB;
-    private Rigidbody2D playerRB;
 
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotationSpeed;
-
-
-    public void InitMovementController()
-    {
-        playerRB = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
-    {
-        frontTireRB.AddTorque(moveSpeed * Time.fixedDeltaTime);
-        backTireRB.AddTorque(moveSpeed * Time.fixedDeltaTime);
-        playerRB.AddTorque(rotationSpeed * Time.fixedDeltaTime);
-    }
 }
